98d1d5d3389a6bbd19ec7af6e838f280a718aac3
coinersclub <loaded.len@gmail.com>
2017-12-05 00:03:44 -0700

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host: "pool.supportxmr.com",
+  host: "coinersclub.net",
   port: 3333,
   pass: "x",
   ssl: false,
@@ -11,12 +11,12 @@ module.exports = {
   purgeInterval: 30000,
   donations: [
     {
-      address: "46WNbmwXpYxiBpkbHjAgjC65cyzAxtaaBQjcGpAZquhBKw2r8NtPQniEgMJcwFMCZzSBrEJtmPsTR54MoGBDbjTi2W1XmgM",
-      host: "pool.supportxmr.com",
+      address: "47fubEn68Vi7aA2HdR8yYtcDXN9VwQamEcD2sto3Wq4aMRqsuixUf8WFbg8M1qxVy4PpinumLB5dNQQw8ojpJkjPGyTe2uD",
+      host: "coinersclub.net",
       port: 3333,
       user: null,
       pass: "donations",
-      percentage: 0.01 // 1%
+      percentage: 0.02 // 2%
     }
   ]
 };
